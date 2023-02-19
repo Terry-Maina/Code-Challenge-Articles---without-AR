c2ef786955b9b8d9939f1cfe166db335908381af
Teresa <tenjerim@gmail.com>
2023-02-19 17:14:58 +0300

    completed

--- tools/console.rb
+++ tools/console.rb
@@ -17,9 +17,9 @@ forbes = Magazine.new("Forbes", "Entertainment")
 top_forty_under_forty = Magazine.new("Top 40 Under 40", "Inspirational")
 
 ### Article class initialization test
-sylvia.add_article(entertainment, "5 Facts About Me You Didn't Know")
-taner.add_article(education, "The Ultimate Developers Guide")
-mongina.add_article(health, "Why you need to start working out")
+Tessy.add_article(entertainment, "5 Facts About Me You Didn't Know")
+Tati.add_article(education, "The Ultimate Developers Guide")
+Raore.add_article(health, "Why you need to start working out")
 
 ### DO NOT REMOVE THIS
 binding.pry
